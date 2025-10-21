86b9eb3a10e7f5812b759cbd4490ab06840e262c
JablesPoles <matheuspolesnunes@gmail.com>
2025-10-21 14:20:38 -0300

    --allow-empty-message

--- infra/terraform/frontend_hosting.tf
+++ infra/terraform/frontend_hosting.tf
@@ -74,13 +74,13 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
     error_caching_min_ttl = 300
     error_code = 403
     response_code = 200
-    response_page_path = "index.html"
+    response_page_path = "/index.html"
   }
   custom_error_response {
     error_caching_min_ttl = 300
     error_code = 404
     response_code = 200
-    response_page_path = "index.html"
+    response_page_path = "/index.html"
   }
 
   restrictions {
